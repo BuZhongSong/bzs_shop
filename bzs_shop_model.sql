b39da67cd96a89a5d1b30619df153551f1da87f8
补中松 <460868361@qq.com>
2016-08-05 18:35:29 +0800

    add bzs_shop sqlmodel

---
+++ bzs_shop_model.sql
@@ -0,0 +1,51 @@
+CREATE TABLE `abon_member` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`nickname` varchar(255) NULL COMMENT '昵称',
+`phone` varchar(25) NOT NULL COMMENT '电话号码',
+`password` varchar(32) NOT NULL COMMENT '密码',
+`created_at` timestamp NOT NULL COMMENT '注册时间',
+`updated_at` timestamp NOT NULL COMMENT '修改时间',
+PRIMARY KEY (`id`) 
+);
+
+CREATE TABLE `abon_category` (
+`id` int(11) NOT NULL,
+`name` varchar(45) NULL COMMENT '名称',
+`sort` int(11) NULL COMMENT '排序',
+`preview` varchar(100) NULL COMMENT '预览图',
+`parent_id` int(11) NULL COMMENT '父组分类',
+`created_at` timestamp NOT NULL COMMENT '创建时间',
+`updated_at` timestamp NOT NULL COMMENT '修改时间',
+PRIMARY KEY (`id`) 
+);
+
+CREATE TABLE `abon_product` (
+`id` int(11) NOT NULL,
+`name` varchar(45) NULL COMMENT '名称',
+`sort` int(11) NULL COMMENT '排序',
+`preview` varchar(100) NULL COMMENT '预览图',
+`parent_id` int(11) NULL COMMENT '父组分类',
+`created_at` timestamp NOT NULL COMMENT '创建时间',
+`updated_at` timestamp NOT NULL COMMENT '修改时间',
+PRIMARY KEY (`id`) 
+);
+
+CREATE TABLE `abon_product_content` (
+`id` int(11) NOT NULL,
+`content` varchar(20000) NULL COMMENT '商品内容',
+`product_id` int(11) NULL COMMENT '商品id',
+`created_at` timestamp NOT NULL,
+`updated_at` timestamp NOT NULL,
+PRIMARY KEY (`id`) 
+);
+
+CREATE TABLE `abon_product_images` (
+`id` int(11) NOT NULL,
+`image_path` varchar(200) NULL COMMENT '图片路径',
+`sort` int(11) NULL COMMENT '排序',
+`product_id` int(11) NOT NULL COMMENT '商品id',
+`created_at` timestamp NOT NULL,
+`updated_at` timestamp NOT NULL,
+PRIMARY KEY (`id`) 
+);
+
